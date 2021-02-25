6d8a7edb81270af0f9888036fb16bd3e2a4b03b3
Justin Tyrrell <justin.d.tyrrell@gmail.com>
2021-02-24 17:43:46 -0700

    cleaning.py and plots.py almost finished

--- src/cleaning.py
+++ src/cleaning.py
@@ -6,15 +6,87 @@ from math import sqrt
 
 asthma_data = pd.read_csv("../data/Colorado_EPHT_Asthma_Hospitalization_Data.csv")
 
-def statewide_high_level_filter(raw_data):
+def remove_uneeded_columns(raw_data):
+    # Removes all, but the five columns i'm using
+    return raw_data[["COUNTY", "RATE", "YEAR", "GENDER", "AGE"]]
+
+
+def statewide_high_level_filter():
     # Filters the data for only statewide rates for both genders, all ages, and between 2010- 2018.
-    county_filter = asthma_data[asthma_data["COUNTY"] == "Statewide"]
+    data = remove_uneeded_columns(asthma_data)
+    county_filter = data[data["COUNTY"] == "Statewide"]
     date_filter = county_filter[county_filter["YEAR"] >= 2010]
     age_filter = date_filter[date_filter["AGE"] == "All ages"]
     gender_filter= age_filter[age_filter["GENDER"] == "Both genders"]
-    gender_filter[["COUNTY", "RATE", "YEAR", "GENDER", "AGE"]].to_csv("../data/statewide_high_level_data.csv")
+    # gender_filter.to_csv("../data/statewide_high_level_data.csv")
+    return gender_filter
+ 
+def high_level_filter_arapahoe_county():
+    # Filters the data for only Arapahoe County rates for both genders, all ages, and between 2010- 2018.
+    data = remove_uneeded_columns(asthma_data)
+    county_filter_arapahoe = data[data["COUNTY"] == "Arapahoe"]
+    date_filter_arapahoe = county_filter_arapahoe[county_filter_arapahoe["YEAR"] >= 2010]
+    age_filter_arapahoe = date_filter_arapahoe[date_filter_arapahoe["AGE"] == "All ages"]
+    gender_filter_arapahoe = age_filter_arapahoe[age_filter_arapahoe["GENDER"] == "Both genders"]
+    return gender_filter_arapahoe
 
+def high_level_filter_arapahoe_county_males():
+    # Filters the data for only Arapahoe County rates for males age 15-34 and between 2010- 2018.
+    data = remove_uneeded_columns(asthma_data)
+    county_filter_arapahoe = data[data["COUNTY"] == "Arapahoe"]
+    date_filter_arapahoe = county_filter_arapahoe[county_filter_arapahoe["YEAR"] >= 2010]
+    age1_filter_arapahoe = date_filter_arapahoe[date_filter_arapahoe["AGE"] == "15-34 years old"]
+    male_filter_arapahoe = age1_filter_arapahoe[age1_filter_arapahoe["GENDER"] == "Male"]
+    return male_filter_arapahoe
+
+def remove_zeroes_and_nans():
+    data = remove_uneeded_columns(asthma_data)
+    # Removes 8,448 rows where RATE was zero
+    minus_rate_zero = data[data["RATE"] != 0] # Removes 8448 rows
+    # Removes rows where RATE is NaN
+    minus_rate_nan = minus_rate_zero.dropna(subset=["RATE"])
+    # Removes rows where AGE is NaN
+    minus_age_nan = minus_rate_nan.dropna(subset=["AGE"])
+    return minus_age_nan
+
+def four_years_pre_legalization():
+    scrubbed_data = remove_zeroes_and_nans()
+    four_years_pre_legalization = scrubbed_data[(scrubbed_data["YEAR"] <= 2013) & 
+                                 (scrubbed_data["YEAR"] >= 2010) &
+                                 (scrubbed_data["COUNTY"] != "Statewide") &
+                                 (scrubbed_data["GENDER"] != "Both genders") &
+                                 (scrubbed_data["AGE"] != "All ages")]
+    return four_years_pre_legalization
+
+def calc_pre_len_mean_variance_std():
+    pre_data = four_years_pre_legalization()
+    pre_len = len(pre_data["RATE"])
+    pre_mean = pre_data["RATE"].mean()
+    pre_variance = pre_data["RATE"].var()
+    pre_std = sqrt(pre_variance)
+    return pre_len, pre_mean, pre_variance, pre_std
+
+def four_years_post_legalization():
+    scrubbed_data = remove_zeroes_and_nans()
+    four_years_post_legalization = scrubbed_data[(scrubbed_data["YEAR"] <= 2017) & 
+                                 (scrubbed_data["YEAR"] >= 2014) &
+                                 (scrubbed_data["COUNTY"] != "Statewide") &
+                                 (scrubbed_data["GENDER"] != "Both genders") &
+                                 (scrubbed_data["AGE"] != "All ages")]
+    return four_years_post_legalization
+
+def calc_post_len_mean_variance_std():
+    post_data = four_years_post_legalization()
+    post_len = len(post_data["RATE"])
+    post_mean = post_data["RATE"].mean()
+    post_variance = post_data["RATE"].var()
+    post_std = sqrt(post_variance)
+    return post_len, post_mean, post_variance, post_std
 
 if __name__ == "__main__":
 
-    print(statewide_high_level_filter(asthma_data))
+    # print(remove_zeroes_and_nans())
+    # print(high_level_filter_arapahoe_county_males())
+    # print(four_years_post_legalization())
+    print(calc_pre_len_mean_variance_std())
+    print(calc_post_len_mean_variance_std())
--- src/plots.py
+++ src/plots.py
@@ -3,18 +3,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 from math import sqrt
-from data.cleaning import statewide_high_level_filter
+from cleaning import statewide_high_level_filter, high_level_filter_arapahoe_county, high_level_filter_arapahoe_county_males, calc_pre_len_mean_variance_std, calc_post_len_mean_variance_std
 
 def statewide_high_level_line_plot(data):
     fig, ax = plt.subplots(dpi=150)
-    plt.plot(gender_filter["YEAR"], gender_filter["RATE"])
+    plt.plot(data["YEAR"], data["RATE"])
     plt.axvline(x=2014, ls="--", color="green", label="Recreational Marijuana Legalization")
-    plt.title("Statewide Hostpitilization Rates From 2010- 2018 for Both Genders and All Ages")
+    plt.title("Statewide Hospitalization Rates From 2010- 2018 for Both Genders and All Ages")
     plt.xlabel("Year")
     plt.ylabel("Asthma Hospitalization Rate")
     plt.legend(loc="best", fontsize="x-small")
-    plt.savefig("../images/Statewide_Hostpitalization_Rates_From_2010_2018_for_Both_Genders_and_All_Ages.png")
+    plt.savefig("../images/Statewide_Hospitalization_Rates_From_2010_2018_for_Both_Genders_and_All_Ages.png")
     return plt
+
+def high_level_filter_arapahoe_county_plot(data):
+    fig, ax = plt.subplots(dpi=150)
+    plt.plot(data["YEAR"], data["RATE"])
+    plt.axvline(x=2014, ls="--", color="green", label="Recreational Marijuana Legalization")
+    plt.title("Arapahoe County Hospitalization Rates From 2010- 2018 for Both Genders and All Ages")
+    plt.xlabel("Year")
+    plt.ylabel("Asthma Hospitalization Rate")
+    plt.legend(loc="best", fontsize="x-small")
+    plt.savefig("../images/Arapahoe_County_Hospitalization_Rates_From_2010_2018_for_Both_Genders_and_All_Ages.png")
+    return plt
+
+def high_level_filter_arapahoe_county_males_plot(data):
+    fig, ax = plt.subplots(dpi=150)
+    plt.plot(data["YEAR"], data["RATE"])
+    plt.axvline(x=2014, ls="--", color="green", label="Recreational Marijuana Legalization")
+    plt.title("Arapahoe County Hospitalization Rates From 2010- 2018 for Males Aged 15- 34")
+    plt.xlabel("Year")
+    plt.ylabel("Asthma Hospitalization Rate")
+    plt.legend(loc="best", fontsize="x-small")
+    plt.savefig("../images/Arapahoe_County_Hospitalization_Rates_From_2010_2018_for_Males_Aged_15_to_34.png")
+    return plt
+
+def condenced_distribution_of_asthma_hospitalization_rates_plot(data1, data2):
+    pre_len, pre_mean, pre_variance, pre_std = data1
+    post_len, post_mean, post_variance, post_std = data2
+
+    pre_norm_dist = stats.norm(pre_mean, pre_std)
+    post_norm_dist = stats.norm(post_mean, post_std)
+    
+    fig, ax = plt.subplots(dpi=150)
+    pre_x = np.linspace(pre_mean - 3*pre_std, pre_mean + 3*pre_std, 100)
+    post_x = np.linspace(post_mean - 3*post_std, post_mean + 3*post_std, 100)
+    plt.title("Distribution of Asthma Hospitalization Rates")
+    plt.xlabel("Hospitalization Rates")
+    plt.ylabel("PDF")
+    plt.axvline(x=pre_mean, ls="--", color="red", label=f"Pre- Mean: {round(pre_mean, 2)}")
+    plt.axvline(x=post_mean, ls="--", color="blue", label=f"Post- Mean: {round(post_mean, 2)}")
+    plt.plot(pre_x, pre_norm_dist.pdf(pre_x), linewidth=2, color="red", label="Pre- Legalization")
+    plt.plot(post_x, post_norm_dist.pdf(post_x), linewidth=2, color="blue", label="Post- Legalization")
+    plt.legend(loc="best")
+    plt.savefig("../images/Condenced_Distribution_of_Asthma_Hospitalization_Rates.png")
+
 
 if __name__ == "__main__":
     statewide_high_level_line_plot(statewide_high_level_filter())
+    high_level_filter_arapahoe_county_plot(high_level_filter_arapahoe_county())
+    high_level_filter_arapahoe_county_males_plot(high_level_filter_arapahoe_county_males())
+    condenced_distribution_of_asthma_hospitalization_rates_plot(calc_pre_len_mean_variance_std(), calc_post_len_mean_variance_std())
